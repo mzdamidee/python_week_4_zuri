7e83b6fa73685631e3c42cb41100a4164536f2bb
Damidee <mzdamidee@gmail.com>
2022-10-28 21:34:49 +0100

    Model field attributes added

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -7,7 +7,9 @@ class Artiste(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     age = models.IntegerField()
-    artiste_id = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.first_name}{self.last_name}"
 
 class Song(models.Model):
     title = models.CharField(max_length=400)
@@ -15,7 +17,16 @@ class Song(models.Model):
     likes = models.IntegerField()
     artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.title}"
+
 class Lyric(models.Model):
-    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    song_id = models.ForeignKey(Song,on_delete = models.CASCADE)
     content = models.CharField(max_length=400)
     song_id = models.IntegerField()
+
+    def __str__(self):
+        if len(self.content) > 50:
+            return f"{self.content[0:50]}..."
+        else:
+            return f"{self.content}"
